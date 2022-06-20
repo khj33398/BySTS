89f29738ddef13252d2f94e6c3ed9e8fe3ebf999
khj33398 <khj33398@naver.com>
2022-06-20 14:54:30 +0900

    init ckeditortest 0620

---
+++ CKEditorTest/src/main/java/com/metanet/ck/CkEditorTestApplication.java
@@ -0,0 +1,13 @@
+package com.metanet.ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkEditorTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkEditorTestApplication.class, args);
+	}
+
+}
